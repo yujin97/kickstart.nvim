c0e53841d4a1d63bcc31d463e107fb3cd250982c
Eugene <eugene97@connect.hku.hk>
2023-05-06 18:24:23 +0900

    feat: add prime's remap and setting

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,5 @@
+require("theprimeagen.remap")
+require("theprimeagen.set")
 function ColorMyPencils()
 	vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
 	vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
