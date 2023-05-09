395b461a5089433ba34e4d73b68cae486f2c498e
Eugene Cheung <eugene.cheung@enigmo.co.jp>
2023-05-09 20:49:38 +0900

    fix: disable COC when the buffer is not in FlowJS project

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -9,3 +9,16 @@ ColorMyPencils()
 
 vim.opt.relativenumber = true
 vim.api.nvim_exec('language en_US', true)
+
+local coc_disable_group = vim.api.nvim_create_augroup('COCDisable', { clear = true })
+vim.api.nvim_create_autocmd('BufAdd', {
+	callback = function()
+		local util = require('lspconfig/util')
+		local bufnr = vim.api.nvim_get_current_buf()
+		if not util.root_pattern(".flowconfig")(vim.fn.getcwd()) then
+			vim.api.nvim_buf_set_var(bufnr, 'coc_enabled', 0)
+		end
+	end,
+	group = coc_disable_group,
+	pattern = '*'
+})
